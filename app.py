70190bff79b8a9d64e28b65456260163dff94e14
Digvijay Sakore <digvijaysakore2005.com>
2024-07-21 16:42:24 +0530

    New

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-import base64
-from flask import Flask, request, jsonify
+import os
+import flask
 from flask_cors import CORS
 from PIL import Image
 from rembg import remove
 import io
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 CORS(app)
 
 @app.route('/process_image', methods=['POST'])
 def process_image():
     try:
-        if 'image' not in request.files:
-            return jsonify({"error": "No image file found"}), 400
+        if 'image' not in flask.request.files:
+            return flask.jsonify({"error": "No image file found"}), 400
 
-        image_file = request.files['image']
+        image_file = flask.request.files['image']
         img = Image.open(image_file.stream)
         img_without_bg = remove(img)
         white_bg = Image.new("RGBA", img_without_bg.size, (255, 255, 255, 255))
@@ -25,13 +25,10 @@ def process_image():
         white_bg_rgb.save(img_io, 'JPEG')
         img_io.seek(0)
 
-        base64_image = base64.b64encode(img_io.getvalue()).decode('utf-8')
-
-        return jsonify({"base64_image": base64_image})
-
     except Exception as e:
         print(f"Error processing image: {e}")
-        return jsonify({"error": str(e)}), 500
+        return flask.jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
